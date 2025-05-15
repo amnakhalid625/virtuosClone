e88c178f8d3fa0b432ba0d8e8eef04357ecb92a2
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-05-15 20:55:03 +0500

    aboutPage

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,30 +1,11 @@
-import React from 'react';
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
-import { Tooltip as ReactTooltip } from 'react-tooltip';
-import mapImage from '../assets/images/mapbg.png';
-
-const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
+import React from 'react'
+import { VectorMap } from "@react-jvectormap/core";
+import { worldMill } from "@react-jvectormap/world";
 
 const Map = () => {
   return (
-    <section style={{ 
-      backgroundImage: `url(${mapImage})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center'
-    }}>
-      <div style={{ width: '100%', height: '100vh' }}>
-        <ComposableMap>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography key={geo.rsmKey} geography={geo} />
-              ))
-            }
-          </Geographies>
-        </ComposableMap>
-      </div>
-    </section>
-  );
-};
+    <div>Map</div>
+  )
+}
 
-export default Map;
+export default Map
