09dc095ff51d4b6fb4c4d1ce275b04303ed0b607
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-05-16 08:38:28 +0500

    mapPage

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -13,9 +13,9 @@ export const Video = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-[#25282A] px-4">
+    <div className="flex justify-center items-center py-8 md:min-h-screen bg-[#25282A] px-4">
       {/* Thumbnail with Play Button */}
-      <div className="relative w-full max-w-3xl rounded-xl overflow-hidden border-none lg:border-2 lg:border-gray-700">
+      <div className="relative w-full max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-3xl rounded-xl overflow-hidden border-none lg:border-2 lg:border-gray-700">
         <img
           src={thumbnail}
           alt="Video Thumbnail"
@@ -25,9 +25,9 @@ export const Video = () => {
           onClick={openModal}
           className="absolute inset-0 flex items-center justify-center focus:outline-none group"
         >
-          <div className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 border-4 border-gray-500 bg-white bg-opacity-10 group-hover:bg-opacity-20 transition-all duration-300 rounded-full flex items-center justify-center backdrop-blur-md shadow-lg">
+          <div className="w-14 h-14 sm:w-16 sm:h-16 md:w-20 md:h-20 border-4 border-gray-500 bg-white bg-opacity-10 group-hover:bg-opacity-20 transition-all duration-300 rounded-full flex items-center justify-center backdrop-blur-md shadow-lg">
             <svg
-              className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 text-white group-hover:scale-110 transform transition-transform duration-300"
+              className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 text-white group-hover:scale-110 transform transition-transform duration-300"
               fill="currentColor"
               viewBox="0 0 24 24"
             >
